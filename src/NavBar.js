013143e82a3ba19c32185c005c9fc9dfd30d2d55
KaramAbuMokh <abu.mokh.karam@gmail.com>
2021-08-27 19:10:11 +0300

    finished navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,34 @@ class NavBar extends React.Component {
     super(props);
   }
   render() {
-    return <div></div>;
+    return this.props.page === "home" ? (
+      <div className="ma3 flex justify-end">
+        <a
+          onClick={() => this.props.changePage("signin")}
+          href="#0"
+          className="f3 ma2 pa2 w4 ba link dim black db"
+        >
+          Logout
+        </a>
+      </div>
+    ) : (
+      <div className="ma3 flex justify-end">
+        <a
+          onClick={() => this.props.changePage("signin")}
+          href="#0"
+          className="f3 ma2 pa2 w4 ba link dim black db"
+        >
+          Signin
+        </a>
+        <a
+          onClick={() => this.props.changePage("register")}
+          href="#0"
+          className="f3 ma2 pa2 w4 ba link dim black db"
+        >
+          register
+        </a>
+      </div>
+    );
   }
 }
 
